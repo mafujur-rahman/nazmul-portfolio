d43f5b4e4dd743231bcad99512d69d1692db07ab
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-07-14 12:40:41 +0600

    Added pricing section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Header from "@/components/Home/Header/Header";
 import Highlight from "@/components/Home/Highlight/Highlight";
 import Portfolio from "@/components/Home/Portfolio/Portfolio";
+import Pricing from "@/components/Home/Pricing/Pricing";
 import Services from "@/components/Home/Services/Services";
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
       <Services />
       <Highlight />
       <Portfolio />
-      
+      <Pricing />
 
     </div>
   );
